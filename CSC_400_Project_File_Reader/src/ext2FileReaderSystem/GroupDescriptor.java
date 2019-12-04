c05bd4d1f714ff1aec349d40104c7a59f93934a8
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2019-12-03 22:11:21 -0500

    Update GroupDescriptor.java
    Comments to GroupDescriptor.java made.

--- CSC_400_Project_File_Reader/src/ext2FileReaderSystem/GroupDescriptor.java
+++ CSC_400_Project_File_Reader/src/ext2FileReaderSystem/GroupDescriptor.java
@@ -20,29 +20,59 @@ public class GroupDescriptor {
     int number_of_directories;
     
     GroupDescriptor(){
-        
+        //default constructor -- no parameters passed as arguments
     }
     
     GroupDescriptor(byte[] location, int groupNumber){
+        // creating a new readfile for components of group descriptor
         ReadFile rf = new ReadFile();
+        
+        //block bitmap location
         block_bitmap_location = rf.processData(location, 0,3);
-            inode_bitmap_location = rf.processData(location, 4,7);
-            inode_table_location = rf.processData(location, 8,11);
-            free_blocks = rf.processData(location, 12,13);
-            free_inodes = rf.processData(location, 14,15);
-            number_of_directories = rf.processData(location, 16,17);
-            group_number = groupNumber;
+        
+        //inode bitmap location
+        inode_bitmap_location = rf.processData(location, 4,7);
+        
+        //inode table location
+        inode_table_location = rf.processData(location, 8,11);
+        
+        //# of free blocks
+        free_blocks = rf.processData(location, 12,13);
+        
+        //# of free inodes
+        free_inodes = rf.processData(location, 14,15);
+        
+        //# of directories
+        number_of_directories = rf.processData(location, 16,17);
+        
+        //setting the object's group_number attribute to the passed groupNumber var
+        group_number = groupNumber;
     }
     
+    //creating a group descriptor table using a superblock and a strating block
     public GroupDescriptor[] createGroupDescriptorTable(SuperBlock superblock, int startingBlock) throws IOException{
+        //creating a new readfile
         ReadFile rf = new ReadFile();
+        
+        //byte array for copy data
         byte[] copyData = new byte[1024];
+        
+        //temp byte array for data read
         byte[] tempDataRead = new byte[1024];
+        
+        //setting gropu count
         int groupCount = (int)Math.ceil(superblock.block_count / superblock.block_per_group);
+        
+        //setting block size for our table
         int blockSizeForTable = (int)Math.ceil((32*groupCount)/superblock.block_size);
+        
+        //setting indiv blocks
         int individualBlocks = superblock.block_size/32;
+        
+        //our list object made with groupCount parameter as an arg
         GroupDescriptor[] tableOfGroupDescriptors = new GroupDescriptor[groupCount];
         
+        //nested loops to copy data and fill table of group descriptors
         for(int i = 0; i < blockSizeForTable; i++){
             tempDataRead = rf.read(superblock.disk, startingBlock+1, tempDataRead);
             for(int j = 0; j < individualBlocks; j++){
